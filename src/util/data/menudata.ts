4d817010aedc78b1f0844a38ec4e8cdcbb83b056
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2025-02-04 22:54:45 +0530

    add product and config menuy

--- src/util/data/menudata.ts
+++ src/util/data/menudata.ts
@@ -9,6 +9,17 @@ export const superAdminMenu = [
         name: 'Dashboard',
         status: 1,
     },
+    {
+        id: 3,
+        name: 'System Configuration',
+        status: 1,
+    },
+
+    {
+        id: 4,
+        name: 'Product Management',
+        status: 1,
+    },
 ];
 
 export const adminMenu = [
