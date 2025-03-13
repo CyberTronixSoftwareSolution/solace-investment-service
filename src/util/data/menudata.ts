768db2b47c8739d581afdbe7e6b0f7bff6e07ecd
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2025-03-14 00:51:05 +0530

    Add report management and various report options to menu data

--- src/util/data/menudata.ts
+++ src/util/data/menudata.ts
@@ -20,6 +20,35 @@ export const superAdminMenu = [
         name: 'Product Management',
         status: 1,
     },
+
+    {
+        id: 13,
+        name: 'Report Management',
+        status: 1,
+    },
+    {
+        id: 17,
+        name: 'Repayment Report',
+        status: 1,
+    },
+
+    {
+        id: 18,
+        name: 'Deduction Charges Report',
+        status: 1,
+    },
+
+    {
+        id: 19,
+        name: 'Investment Report',
+        status: 1,
+    },
+
+    {
+        id: 20,
+        name: 'Daily Manager Report',
+        status: 1,
+    },
 ];
 
 export const adminMenu = [
@@ -31,6 +60,33 @@ export const adminMenu = [
     {
         id: 2,
         name: 'Dashboard',
+        status: 1,
+    },
+    {
+        id: 13,
+        name: 'Report Management',
+        status: 1,
+    },
+
+    {
+        id: 13,
+        name: 'Report Management',
+        status: 1,
+    },
+    {
+        id: 17,
+        name: 'Repayment Report',
+        status: 1,
+    },
+
+    {
+        id: 18,
+        name: 'Deduction Charges Report',
+        status: 1,
+    },
+    {
+        id: 19,
+        name: 'Investment Report',
         status: 1,
     },
 ];
